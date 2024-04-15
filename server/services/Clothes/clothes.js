564cde381927e3bccf8500cae1208147139c707f
KiteKite <kite.dtk@gnail.com>
2024-04-15 10:14:10 +0700

    cloth

---
+++ server/services/Clothes/clothes.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function getAllClothes(){
+    const clothes = await prisma.clothes.findMany({
+        include:{
+            clothDetails : true
+        }
+    });
+    return clothes;
+}
+async function getSingleClothes(clothesId){
+    const cloth = await prisma.clothes.findUnique({
+        where:{
+            id: clothesId
+        },
+        include:{
+            clothDetails: true
+        }
+    });
+    return cloth
+}
+module.exports ={
+    getAllProduct, getSingleClothes
+}
