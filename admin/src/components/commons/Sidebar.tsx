af01ac7705bb2a62010997ac1c900e43ed4bc7f8
Kite170901 <varussvg@gmail.com>
2024-09-19 22:53:47 +0700

    fix

--- admin/src/components/commons/Sidebar.tsx
+++ admin/src/components/commons/Sidebar.tsx
@@ -11,14 +11,15 @@ export function Sidebar() {
             className="brand-image img-circle elevation-3"
             style={{ opacity: 0.8 }}
           />
-          <span className="brand-text font-weight-light">AdminLTE 3</span>
+          <span className="brand-text font-weight-light">KITESHOP</span>
         </a>
 
         <div className="sidebar">
           <div className="user-panel mt-3 pb-3 mb-3 d-flex">
             <div className="image">
               <img
-                src="dist/img/user2-160x160.jpg"
+                src="dist/img/image.png"
+                style={{width: 30, height: 30}}
                 className="img-circle elevation-2"
                 alt="User Image"
               />
@@ -31,19 +32,7 @@ export function Sidebar() {
           </div>
 
           <div className="form-inline">
-            <div className="input-group" data-widget="sidebar-search">
-              <input
-                className="form-control form-control-sidebar"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <div className="input-group-append">
-                <button className="btn btn-sidebar">
-                  <i className="fas fa-search fa-fw"></i>
-                </button>
-              </div>
-            </div>
+
           </div>
 
           <nav className="mt-2">
